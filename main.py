336ecd6abe79e103a6d13f242278d9c9211da9b1
kyujin <dadanoac@gmail.com>
2022-04-14 15:09:24 +0900

    get/post

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
 from typing import List
 
 from fastapi import FastAPI, Query, Path
-from pydantic import BaseModel, parse_obj_as
+from pydantic import BaseModel, Field
+import uvicorn
 
 app = FastAPI()
-
-inventory = (
-    {
-        "id": 1,
-        "user_id": 1,
-        "name": "레전드포션",
-        "price": 2500.0,
-        "amount": 100,
-    },
-    {
-        "id": 2,
-        "user_id": 1,
-        "name": "포션",
-        "price": 300.0,
-        "amount": 50,
-    },
-)
-
+    
+inventory = []
 
 class Item(BaseModel):
-    name: str
-    price: float
-    amount: int = 0
-
+    name: str = Field(..., min_length=1, max_length=100, title="이름")
+    price: float = Field(None, ge=0)
+    amount: int = Field(
+        default=1,
+        gt=0,
+        le=100,
+        title="수량",
+        description="아이템 갯수. 1~100 개 까지 소지 가능",
+    )
 
 @app.get("/users/{user_id}/inventory", response_model=List[Item])
 def get_item(
@@ -48,3 +38,11 @@ def get_item(
             response.append(item)
 
     return response
+
+@app.post("/users/{user_id}/item")
+def create_item(item: Item):
+    inventory.append(item)
+    return item
+
+if __name__ == "__main__":
+    uvicorn.run(app = "main:app")
